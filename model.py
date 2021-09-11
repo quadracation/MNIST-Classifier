dc184724c712308981e445531547bd53430ea4ba
quadracation <ben.cz.cao@gmail.com>
2021-09-11 15:39:59 -0700

    Altered score to F-Score

--- model.py
+++ model.py
@@ -5,13 +5,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score
 import numpy as np
 import pandas as pd
 
 
 
-
-data = fetch_openml('mnist_784', data_home = "C:/Users/neb/Desktop/git/")
+## use openml to fetch dataset, presplit between data and target 
+data = fetch_openml('mnist_784')
 model = make_pipeline(StandardScaler(), SVC(gamma= 'auto'))
 
 print("Data Loaded\n")
@@ -20,9 +21,11 @@ print("Data Loaded\n")
 X= data.data 
 y = data.target
 
+##Double checking number of samples and shape of object
 print("length of data arr: ",len(X))
 print("length of label arr: ",len(y))
-print("shape of data X,y", X.shape, y.shape )
+##Should be 784 features
+print("shape of data X,y", X.shape, y.shape)
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.33, shuffle = True)
@@ -30,6 +33,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.33, shuff
 
 
 model.fit(X_train, y_train)
+#returns y_pred as an array
+predict = model.predict(X_test)
+
+#Performing f1_score calculation prior to cross validation
+f1 = f1_score(y_test, predict, average = "micro")
+
+print("F1 Score with test size = 23100:", f1)
 
 ##Cross Validating data and keeping metrics
 scores = cross_val_score(model, X, y, scoring= 'f1_micro')
@@ -37,7 +47,7 @@ scores = cross_val_score(model, X, y, scoring= 'f1_micro')
 
 #printing scores
 
-print("Avg Accuracy: ", np.mean(scores))
+print("Avg F1 After 5 fold validation: ", np.mean(scores))
 
 
 
